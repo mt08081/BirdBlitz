d60968c2b493a8e40bad72f28b80e0942601fb2e
AbdulNafay <175629338+AbdulNafay-Saqib@users.noreply.github.com>
2024-07-22 20:16:29 +0500

    Making the home page
    just makin the home page

---
+++ Assets/Scripts/Home.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Home : MonoBehaviour
+{
+   public void OnPlayButton ()
+    {
+        SceneManager.LoadScene ("SampleScene");
+    }
+}
