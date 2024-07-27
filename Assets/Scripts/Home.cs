3a38936ca9763f1dcde399bb90d6d5e1d9b9d0f3
AbdulNafay <175629338+AbdulNafay-Saqib@users.noreply.github.com>
2024-07-27 17:00:54 +0500

    wroking on main menu

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -9,4 +9,9 @@ public class Home : MonoBehaviour
     {
         SceneManager.LoadScene ("SampleScene");
     }
+
+    public void OnShopButton ()
+    {
+        SceneManager.LoadScene ("Store");
+    }
 }
