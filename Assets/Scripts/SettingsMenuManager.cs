f3d0d285dda51d56828908c2ebcb0c14a152fcf0
AbdulNafay <175629338+AbdulNafay-Saqib@users.noreply.github.com>
2024-08-12 22:15:02 +0500

    settings scene

---
+++ Assets/Scripts/SettingsMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class SettingsMenuManager : MonoBehaviour
+{
+    public TMP_Dropdown graphicsDropdown;
+
+
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown,value);
+    }
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
