9cb5bc7c0b922e8cf7ece500f97ca1e9820d03fc
AbdulNafay <175629338+AbdulNafay-Saqib@users.noreply.github.com>
2024-08-12 22:35:33 +0500

    Just deleted the settings scene

--- Assets/Scripts/SettingsMenuManager.cs
+++ Assets/Scripts/SettingsMenuManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-
 public class SettingsMenuManager : MonoBehaviour
 {
     public TMP_Dropdown graphicsDropdown;
 
 
+
     public void ChangeGraphicsQuality()
     {
-        QualitySettings.SetQualityLevel(graphicsDropdown,value);
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
     }
 
     void Start()
